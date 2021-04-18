7b815c92396e76b271a862a6535bf60ce215ec27
Cyriacus Ifeanyi <cyriacusifeanyi@gmail.com>
2021-04-18 16:28:51 +0100

    completed 8.3: All authors

--- index.js
+++ index.js
@@ -95,18 +95,36 @@ const typeDefs = gql`
     id: ID!
   }
 
+  type Author {
+    name: String!
+    born: String
+    bookCount: Int!
+    id: ID!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
-
 const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors,
+  },
+  Author: {
+    bookCount: (root) => {
+      const getBookCount = (array, value) => {
+        var count = 0
+        array.forEach(element => element.author === value && count++)
+        return count
+      }
+      return getBookCount(books, root.name)
+    }
   },
 }
 
