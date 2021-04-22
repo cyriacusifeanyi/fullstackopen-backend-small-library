6de93c9b3f707ad91a166e41457aed7f188881f6
Cyriacus Ifeanyi <cyriacusifeanyi@gmail.com>
2021-04-22 12:43:05 +0100

    completed 8.7: Updating the birth year of an author

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ const typeDefs = gql`
 
   type Author {
     name: String!
-    born: String
+    born: Int
     bookCount: Int!
     id: ID!
   }
@@ -119,6 +119,10 @@ const typeDefs = gql`
       published: Int!
       genres: [String!]
     ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `
 const resolvers = {
@@ -162,7 +166,17 @@ const resolvers = {
       const book = { ...args, id: uuid() }
       books = books.concat(book)
       return book
-    }
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(author => author.name === args.name)
+      if (!author) {
+        return null
+      }
+  
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map(author => author.name === args.name ? updatedAuthor : author)
+      return updatedAuthor
+    },
   }
 }
 
